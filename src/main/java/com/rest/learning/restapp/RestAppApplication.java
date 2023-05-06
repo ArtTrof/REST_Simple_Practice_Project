ae0d6fea67679d24529fc1cb8f3adb739bf8462d
Artem Trofymenko <randallsng@gmail.com>
2023-05-06 20:43:51 +0200

    config change

--- src/main/java/com/rest/learning/restapp/RestAppApplication.java
+++ src/main/java/com/rest/learning/restapp/RestAppApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestAppApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestAppApplication.class, args);
 	}
